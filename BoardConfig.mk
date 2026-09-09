66f5b126e7c94d998ce4500b86c8318394c56080
ibadriansyah <ibadriansyah1998@gmail.com>
2026-09-09 13:22:04 +0700

    BoardConfig.mk: add target_no_recovery as true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,13 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := redwood
 
+# Boot Image
+BOARD_BOOTIMAGE_PARTITION_SIZE := 201326592
+
+# Recovery / Boot Image Configuration
+BOARD_USES_RECOVERY_AS_BOOT := true
+TARGET_NO_RECOVERY := true
+
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm7325
 TARGET_KERNEL_CONFIG := vendor/xiaomi-qgki_defconfig vendor/redwood.config
